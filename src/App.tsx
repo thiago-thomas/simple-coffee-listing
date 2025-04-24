1467240ea5d6ab3d30ccc9e2b9258bfab923a6ae
Thiago Thomas <thiago_rocha17@hotmail.com>
2025-04-24 16:29:14 +0100

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-yellow-700 font-bold text-5xl'>Simple Coffee Listing</h1>
+    </div>
+  )
+}
+
+export default App
