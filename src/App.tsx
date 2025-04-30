c3136847681b53fa1a9cc9c72f1f64ea1fe997f9
Thiago Thomas <thiago_rocha17@hotmail.com>
2025-04-30 21:44:50 +0100

    feat: creating coffee component with value, avaliations and votes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import CoffeeList from './CoffeeList'
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <h1>Our Collection</h1>
-      <p>
-        Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins expertly roasted in small batches and shipped fresh weekly
-      </p>
+    <div className='bg-amber-950 m-8 rounded-lg'>
+      <div className='text-center mb-2 mt-8 px-8'>
+        <h1 className='text-4xl'>Our Collection</h1>
+        <p className='text-lg text-gray-600'>
+          Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins expertly roasted in small batches and shipped fresh weekly
+        </p>
+      </div>
       <div>
         <button>All Products</button>
         <button>Available Now</button>
