285e15eb8bb05ec049c46ff60bd917f55d90ed01
Thiago Thomas <thiago.santos@truechange.com.br>
2025-05-09 20:07:09 +0100

    feat: update Card style

--- src/Card.tsx
+++ src/Card.tsx
@@ -10,16 +10,17 @@ interface Coffee {
 
 export default function Card({ coffee }: { coffee: Coffee }) {
   return (
-    <div>
+    <div className="relative">
       <img src={coffee.image} alt={coffee.name} className="object-cover mb-1 rounded-lg" />
-      <div className="flex justify-between items-center mb-1">
-        <h2 className="text-xl font-bold">{coffee.name}</h2>
-        <p className="text-lg font-semibold">{coffee.price}</p>  
+      <div className="flex justify-between items-center mb-2 mt-4">
+        {coffee.popular && <span className="absolute top-[10px] left-[10px] bg-amber-300 text-black px-3 py-1 text-xs font-semibold rounded-xl">Popular</span>}
+        <h2 className="text-xl font-semibold">{coffee.name}</h2>
+        <p className="bg-[#bee3cc] text-[#2d3232] text-md font-bold px-2 py-1 rounded-sm">{coffee.price}</p>  
       </div>
       <div className="flex items-center gap-1">
         <span>⭐</span>
         <span className="text-lg font-bold">{coffee.rating}</span>
-        <span className="text-base font-semibold">({coffee.votes} votes)</span>  
+        <span className="text-lg font-semibold text-[#63686f]">({coffee.votes} votes)</span>  
       </div>
     </div>
   )
